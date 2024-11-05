5e34bc55749939850331add66e615f07eb17bd93
ethsen <ethansenatore@gmail.com>
2024-11-05 15:04:37 -0500

    update

--- HW 3/code/lse.py
+++ HW 3/code/lse.py
@@ -1,9 +1,14 @@
 import numpy as np
 
-def least_squares_estimation(X1, X2):
-  """ YOUR CODE HERE
-  """
+def least_squares_estimation(X1, X2): 
+  c1 = X2[:,0][:,np.newaxis] * X1
+  c2 = X2[:,1][:,np.newaxis] * X1
+  c3 = X2[:,2][:,np.newaxis] * X1
+  A = np.hstack([c1,c2,c3])
+  u, s, vT = np.linalg.svd(A, full_matrices= True)
+  E = vT[-1].reshape(3,3)
+  u,s, vT = np.linalg.svd(E)
+  #E = u @ np.diag([(s[0] + s[1])/2 , (s[0] + s[1])/2 , 0]) @ vT
+  E = u @ np.diag([1 , 1 , 0]) @ vT
 
-  """ END YOUR CODE
-  """
   return E
--- HW 3/code/ransac.py
+++ HW 3/code/ransac.py
@@ -9,20 +9,37 @@ def ransac_estimator(X1, X2, num_iterations=60000):
     best_num_inliers = -1
     best_inliers = None
     best_E = None
+    e3 = np.array([[0,-1,0],
+                   [1,0,0],
+                   [0,0,0]])
 
     for i in range(num_iterations):
         # permuted_indices = np.random.permutation(np.arange(X1.shape[0]))
         permuted_indices = np.random.RandomState(seed=(i*10)).permutation(np.arange(X1.shape[0]))
         sample_indices = permuted_indices[:sample_size]
         test_indices = permuted_indices[sample_size:]
-
         """ YOUR CODE HERE
         """
+        E = least_squares_estimation(X1[sample_indices], X2[sample_indices])
+
+        numerator = (X2[test_indices] @ (E @ X1[test_indices].T))**2
+        denominator = np.linalg.norm((e3 @ (E @ X1[test_indices].T)), axis= 0)**2
+        d1 =  np.diag(numerator /denominator)
+
+        numerator = (X1[test_indices] @ (E @ X2[test_indices].T))**2
+        denominator=np.linalg.norm((e3 @ (E.T @ X2[test_indices].T)), axis= 0)**2
+        d2 = np.diag(numerator/denominator)
+        d = d1 
+        #numerator = (np.sum(X2[test_indices]  * (X1[test_indices] @ E), axis=1))**2
+        #denominator = np.sum((X1[test_indices] @ E.T)[:,:2]**2, axis= 1) + np.sum((E @ X1[test_indices].T)[:2,:]**2, axis = 0)
+        #d = numerator /denominator
+        inliers = test_indices[d< eps]
+        num_inliers = len(inliers)
 
         """ END YOUR CODE
         """
-        if inliers.shape[0] > best_num_inliers:
-            best_num_inliers = inliers.shape[0]
+        if num_inliers > best_num_inliers:
+            best_num_inliers = num_inliers
             best_E = E
             best_inliers = inliers
 
